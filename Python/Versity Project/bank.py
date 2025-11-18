d0f0cf9a61512ac8d8486de7ca884c4307295f2e
RT Jeion <rejuwan002@gmail.com>
2025-11-18 18:17:20 +0600

    Create bank.py

---
+++ Python/Versity Project/bank.py
@@ -0,0 +1,30 @@
+bank = {
+    "rt_jeion":{
+        "name":"",
+        "password":"",
+        "balance":0000,
+        "history":[]
+    }
+}
+
+def create_account():
+    pass
+
+def access():
+    pass
+
+def check_balance():
+    pass
+
+def check_history():
+    pass
+
+def modify_account():
+    pass
+
+
+if __name__ == "__main__":
+    act = input("Enter your action you want to do?\nCreatr account type[create]")
+    while True:
+        if 
+      
